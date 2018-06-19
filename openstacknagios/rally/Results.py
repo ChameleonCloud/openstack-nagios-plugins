7710afd4b70d22bfa12387242f9393a45f112ba1
Jason Anderson <jasonanderson@uchicago.edu>
2018-06-19 17:44:02 -0500

    Update rally checks to use new session-based auth

--- openstacknagios/rally/Results.py
+++ openstacknagios/rally/Results.py
@@ -14,7 +14,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#  
+#
 
 """
   Nagios/Icinga plugin to check rally results.
@@ -22,7 +22,6 @@
   Takes the output of 'rally task results' as input on stdin.
   and calculates the sum of load- and full_duration and the number
   of failed scenarios.
-
 """
 
 import openstacknagios.openstacknagios as osnag
@@ -33,22 +32,20 @@ class RallyResults(osnag.Resource):
     """
     """
 
-    def __init__(self, args=None):
-        if args.resultfile:
-            infile = open(args.resultfile, 'r')
-
+    def __init__(self, result_file=None, args=None):
+        if result_file:
+            infile = open(result_file, 'r')
             with infile:
                 try:
-                    self.results=json.load(infile)
+                    self.results = json.load(infile)
                 except ValueError:
                     raise SystemExit(sys.exc_info()[1])
         else:
-            self.results=json.load(sys.stdin)
+            self.results = json.load(sys.stdin)
 
-        osnag.Resource.__init__(self)
+        osnag.Resource.__init__(self, args)
 
     def probe(self):
-
         full_duration=0
         load_duration=0
         total=0
@@ -67,7 +64,6 @@ class RallyResults(osnag.Resource):
                     if not sla['success']:
                         slafail=slafail+1
 
-
         yield osnag.Metric('total', total)
         yield osnag.Metric('errors', errors )
         yield osnag.Metric('slafail', slafail )
@@ -78,7 +74,7 @@ class RallyResults(osnag.Resource):
 def main():
     argp = osnag.ArgumentParser(description=__doc__)
 
-    argp.add_argument('--resultfile',
+    argp.add_argument('--result_file',
                       help='file to read results from (output of rally task results) if not specified, stdin is used.' )
 
     argp.add_argument('-w', '--warn', metavar='RANGE', default=':0',
@@ -109,17 +105,14 @@ def main():
     args = argp.parse_args()
 
     check = osnag.Check(
-        RallyResults(args=args),
+        RallyResults(result_file=args.result_file, args=args),
         osnag.ScalarContext('errors', args.warn, args.critical),
         osnag.ScalarContext('total', args.warn_total, args.critical_total),
         osnag.ScalarContext('slafail', args.warn_slafail, args.critical_slafail),
         osnag.ScalarContext('fulldur', args.warn_fulldur, args.critical_fulldur),
         osnag.ScalarContext('loaddur', args.warn_loaddur, args.critical_loaddur),
-        osnag.Summary(show=['errors','slafail'])
-        )
+        osnag.Summary(show=['errors','slafail']))
     check.main(verbose=args.verbose, timeout=args.timeout)
 
 if __name__ == '__main__':
     main()
-
-
